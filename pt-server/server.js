a393d3824efcf7d7862896a20cf6e46e3aa3843a
bradleybutler247 <bradleybutler247@proton.me>
2024-09-17 19:13:46 -0600

    update server.js

--- pt-server/server.js
+++ pt-server/server.js
@@ -4,7 +4,11 @@ const { PORT } = require("./config");
 
 
 
-app.listen(PORT, () => {
-  console.log(`Started on http://localhost:${PORT}`)
-});
+// app.listen(PORT, () => {
+//   console.log(`Started on http://localhost:${PORT}`)
+// });
 
+const server = app.listen(PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
