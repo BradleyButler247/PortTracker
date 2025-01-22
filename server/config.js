d5738a5f6e8cb600778303d65f8db02f625155ba
bradleybutler247 <bradleybutler247@proton.me>
2025-01-21 18:23:48 -0700

    update port

--- server/config.js
+++ server/config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 require("colors"); 
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const PORT = process.env.PORT || 6969;
+const PORT = process.env.PORT || 5432;
 
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
